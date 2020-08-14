b14a6537218871761139bc9187e47174e88126cb
Antônio Oliveira <antonio.filho128@gmail.com>
2020-08-14 04:10:22 -0300

    Initial commit

---
+++ cdi.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+# CDI - Change Dir Interactively
+# Don't waste more time in the terminal browsing folders with CD
+
+# TODO: Change directory
+
+##################### Functions ################################
+
+print_folders() {
+    # $1 = current_dir
+    # $2 = current_selection
+
+    echo ">> $1 $2 <<"
+
+    array=($(ls -p $1 | grep /))
+    for i in "${!array[@]}"; do
+        if test "$i" -eq $2
+        then
+            echo "→ ${array[i]}"
+        else
+            echo "  ${array[i]}"
+        fi
+    done
+}
+
+get_selected_folder() {
+    # $1 = current_dir
+    # $2 = current_selection
+
+    array=($(ls -p $1 | grep /))
+    for i in "${!array[@]}"; do
+
+        if test "$i" -eq $2
+        then
+            selected_folder=${array[i]}
+        fi
+    done
+}
+
+print_instructions() {
+    echo -e '\nUse arrows to move ↑ ↓ ← →\n'
+}
+###############################################################
+
+# Initial values
+current_dir=$(pwd)
+current_selection=0
+
+# Hide cursor
+tput civis
+
+# Initial instructions
+clear
+
+escape_char=$(printf "\u1b")
+while 
+    print_instructions
+    print_folders $current_dir $current_selection
+    read -rsn1 mode
+    clear
+    do
+
+    if [[ $mode == $escape_char ]]; then
+        read -rsn2 mode # read 2 more chars
+    fi
+
+    echo $mode
+
+    case $mode in
+        'q') echo QUITTING ; exit ;;
+        '[A') current_selection=$((current_selection-1)) ;; # UP
+        '[B') current_selection=$((current_selection+1)) ;; # DOWN
+        
+        '[D') # LEFT
+            current_dir="$current_dir/.."
+            current_selection=0
+        ;; 
+        
+        '[C') # RIGHT
+            get_selected_folder $current_dir $current_selection
+            current_dir="$current_dir/$selected_folder"
+            current_selection=0
+        ;;
+
+        *) >&2 
+            echo 'saindo...'; 
+            return ;;
+    esac
+done
